ff8479216631a62fcd9e80ccdfcd5576d1f69e4b
kotopiro <mnxsv69789@yahoo.co.jp>
2026-02-11 18:55:21 +0900

    Refactor Bot initialization and on_ready event

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ intents = discord.Intents.all()
 
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(
-            command_prefix="!",
-            intents=intents
-        )
+        super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
         for file in os.listdir("./cogs"):
@@ -21,9 +18,7 @@ bot = Bot()
 
 @bot.event
 async def on_ready():
-    print(f"最強Bot起動: {bot.user}")
-    await bot.change_presence(
-        activity=discord.Game("/help | school-rekisi.kesug.com")
-    )
+    print(f"起動: {bot.user}")
+    await bot.change_presence(activity=discord.Game("/help | school-rekisi.kesug.com"))
 
 bot.run(os.getenv("TOKEN"))
